13457f6a21f108cca1c6b448d31328ff15ff090c
Tonja <tonja64@gmail.com>
2018-05-14 19:48:09 -0700

    added a laugh

--- shortStory.rb
+++ shortStory.rb
@@ -2,3 +2,5 @@ Here's a little story about how my life was
 turned upside down. Go into a fight with some
 people in the neighborhood.... got sent to my 
 uncle and auntie in Bel Air
+Fresh Prince of Bel Air 
+haha 
